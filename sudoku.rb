cb75b97bbf64f67602a7a4a41c2f0ce65214ef60
reynier25 <fsoto@uchicago.edu>
2019-11-18 14:38:50 -0800

    game file reader

---
+++ sudoku.rb
@@ -0,0 +1,35 @@
+require_relative "board"
+
+class SudokuGame
+  def self.from_file(filename)
+    board = Board.from_file(filename)
+    self.new(board)
+  end
+
+  def initialize(board)
+    @board = board
+  end
+
+  def get_pos
+    pos = nil
+    until pos && valid_pos?(pos)
+      puts "Please enter a position on the board (e.g., '3,4')"
+      print "> "
+      pos = parse_pos(gets.chomp)
+    end
+    pos
+  end
+
+  def get_val
+    val = nil
+    until val && valid_val?(val)
+      puts "Please enter a value between 1 and 9 (0 to clear the tile)"
+      print "> "
+      val = parse_val(gets.chomp)
+    end
+    val
+  end
+
+
+end
+
